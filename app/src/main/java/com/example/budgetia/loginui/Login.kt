7a0f138154e14294d717bef98d2dfce02b730b7c
brianwangocho <bwangocho@gmail.com>
2020-07-18 19:50:13 +0300

    houseKeeping

--- app/src/main/java/com/example/budgetia/loginui/Login.kt
+++ app/src/main/java/com/example/budgetia/loginui/Login.kt
@@ -1,39 +1,51 @@
 package com.example.budgetia.loginui
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.databinding.DataBindingUtil
+import android.util.Log
 import androidx.lifecycle.ViewModelProviders
-import com.example.budgetia.Dashboard
 import com.example.budgetia.R
-import com.example.budgetia.databinding.ActivityLoginBinding
-import com.example.budgetia.expenseui.ExpenseViewModel
 import com.example.budgetia.roomdatabase.MyRoomDatabase
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
-    private lateinit var binding:ActivityLoginBinding
+
     private lateinit var userViewModel: UserViewModel
+
+    private  val TAG = "LoginActivity"
+    private  var username: TextInputLayout? = null
+    private  var password: TextInputLayout? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
-        val userDao = MyRoomDatabase.getDatabase(application).userDao()
+         username = findViewById(R.id.login_username) as TextInputLayout
+         password = findViewById(R.id.login_password) as TextInputLayout
+
 
         val factory =  UserViewModelFactory(application)
         userViewModel = ViewModelProviders.of(this,factory).get(UserViewModel::class.java)
-        binding.userviewmodel = userViewModel
-        binding.lifecycleOwner=this
+
+        loginButton.setOnClickListener {
+            if(validateInputs()){
+
+            }
+
+        }
+
+    }
+
+    private  fun validateInputs():Boolean{
+        val c = username.toString().trim()
+        Log.d(TAG,"username is  $username")
 
 
 
-
-
-
+        return true
 
     }
 
--- app/src/main/java/com/example/budgetia/loginui/UserViewModel.kt
+++ app/src/main/java/com/example/budgetia/loginui/UserViewModel.kt
@@ -1,34 +1,17 @@
 package com.example.budgetia.loginui
 
 import android.app.Application
-import androidx.databinding.Bindable
-import androidx.lifecycle.MutableLiveData
+
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+
 import com.example.budgetia.Repository.UserRepository
 import com.example.budgetia.roomdatabase.MyRoomDatabase
-import kotlinx.coroutines.launch
+
 
 class UserViewModel(application: Application):ViewModel(){
     val userDao = MyRoomDatabase.getDatabase(application).userDao()
     private var userRepository = UserRepository(userDao)
 
 
-    @Bindable
-    val inputEmail = MutableLiveData<String>()
-
-    @Bindable
-    val inputPassword = MutableLiveData<String>()
-
-    @Bindable
-    val loginButtonText = MutableLiveData<String>()
-
-    init{
-        loginButtonText.value="login"
-    }
-
-    fun login(email:String,password:String)=viewModelScope.launch {
-        userRepository.loginUser(email,password)
-    }
 
 }
